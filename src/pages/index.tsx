4a5f0be1666691df05f48d4195165d3e15bdc60c
Pablo <pablolucio_@hotmail.com>
2022-07-04 12:59:31 -0300

    IntroductionSection created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Container } from '../styles'
+import { NextImage } from '../components/Next/NextImage'
+import { Title } from '../components/Typography/Title'
+import { Text } from '../components/Typography/Text'
+import { BannerSection, BannerSectionContainer, Container, IntroductionSection, IntroductionSectionContainer, IntroductionSectionContentContainer, IntroductionSectionImageContainer } from '../styles'
 
 const Home: NextPage = () => {
 
   return (
-    <Container style={{marginBottom: 120}}>
+    <Container style={{ marginBottom: 120 }}>
       <Head>
         <title> MyPage | Home</title>
         {/* @ts-ignore */}
@@ -15,6 +18,33 @@ const Home: NextPage = () => {
         <meta name='author' content='Pablo Silva' />
         <meta name="theme-color" content="#000000" />
       </Head>
+      <BannerSection id='home'>
+        <BannerSectionContainer />
+      </BannerSection>
+      <IntroductionSection>
+        <IntroductionSectionContainer>
+          <IntroductionSectionImageContainer>
+            <NextImage
+              height={576}
+              width={384
+              }
+              imgUrl='/woman.png'
+            />
+          </IntroductionSectionImageContainer>
+        
+          <IntroductionSectionContentContainer>
+          <Title 
+          content='Quem sou eu'
+          />
+          <Text
+          content='Meu nome é Simone Alvarenga sou formada em psicologia pela Universidade de São Paulo e venho trabalhando como psicóloga há mais de 17 anos.'
+           />
+          <Text
+          content='Com o propósito de ajudar o máximo de pessoas possíveis, me especializei no estudo da mente humana. Hoje atuo com diversos tratamento que auxiliam na recuperação de experiências traumáticas e psicologicamente prejudiciais.'
+           />
+          </IntroductionSectionContentContainer>
+        </IntroductionSectionContainer>
+      </IntroductionSection>
     </Container>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,17 +4,17 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  background-color: ${({ theme }) => theme.colors.white100};
 `
-/* BANNER WITH BACKGROUND IMAGE USAGE */
 
-/* export const BannerSection = styled.section`
+
+export const BannerSection = styled.section`
   display: flex;
   width: 100%;
   min-height: 120vh;
 
   @media(max-width: 1440px){
      min-height: 90vh;
-    background-color: red;
   } 
   @media(max-width: 1080px){
     min-height: 56vh;
@@ -27,21 +27,52 @@ export const Container = styled.div`
 export const BannerSectionContainer = styled.div`
   width: 100%;
   margin: 0 auto;
-  background-image: url("/images/banner-contact.png");
+  background-image: url("/banner.png");
   background-repeat: no-repeat;
   background-size: cover;
-`; */
-
-export const BannerSection = styled.section`
-  display: flex;
 `;
 
 export const IntroductionSection = styled.section`
   display: flex;
+  width: 100%;
+  min-height: 32vh;
 `;
 
-export const MetricsSection = styled.section`
+export const IntroductionSectionContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 1900px;
+    padding: 40px;
+    margin: 0 auto;
+    @media(max-width: 720px){
+      width: 100%;
+      flex-direction: column;
+    }
+    `;
+export const IntroductionSectionImageContainer = styled.div`
+    display: flex;
+    width: 50%;
+    @media(max-width: 720px){
+      width: 100%;
+      flex-direction: column;
+    }
+    `;
+export const IntroductionSectionContentContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 50%;
+  padding-right: 32px;
+  @media(max-width: 720px){
+    width: 100%;
+    padding-right: 0;
+    flex-direction: column;
+    margin-top: 24px;
+  }
+  `;
+
+export const MetricsSection = styled.section`
+
 `;
 
 export const TestimonialsSection = styled.section`
@@ -56,13 +87,10 @@ export const PurchaseSection = styled.section`
   display: flex;
 `;
 
-export const BannerSectionContainer = styled.div`
-  display: flex;
-`;
 
-export const IntroductionSectionContainer = styled.div`
-  display: flex;
-`;
+
+
+
 
 export const MetricsSectionContainer = styled.div`
   display: flex;
